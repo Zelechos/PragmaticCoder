4b27b0dc77d8857ecf409531abc813441e6fec97
Tumiri Huanca Alex <zelechos@hotmail.com>
2020-10-10 09:14:42 -0400

    Version 0.0.1

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function toggle(){
+    let sec = document.getElementById('sec');
+    let nav = document.getElementById('navigation');
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
+}
