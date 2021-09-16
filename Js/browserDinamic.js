e54b93b40ec4f9a516ef6a1933072b97ef0944a9
Zelechos <zelechos@hotmail.com>
2021-09-15 20:13:39 -0400

    v0.0.5

--- Js/browserDinamic.js
+++ Js/browserDinamic.js
@@ -8,8 +8,6 @@ function browser(){
     document.addEventListener("wheel",()=>{
         let Top = browser.scrollTop;
         var pageactual = page.offsetTop;
-        console.log("pageactual ->",pageactual);
-        console.log("Top->",Top);
 
             // if(Top < 690){
             //     page.style.position = 'initial';
@@ -31,11 +29,10 @@ var ticking = false;
 
 function doSomething(scroll_pos) {
     console.log("position scroll -> ",scroll_pos);
-    if(scroll_pos < 1896){
+    if(scroll_pos < 1900){
         page.style.display = 'initial';
         page.style.position = 'fixed';
         page.style.width = "49%";
-
         page.style.marginTop = "270px";
         
         finalpage.style.backgroundColor = "initial";
@@ -49,8 +46,9 @@ function doSomething(scroll_pos) {
         page.style.marginTop = "0px";
     }
 
-    if(scroll_pos > 1897){
+    if(scroll_pos > 1900){
         page.style.display = 'none';
+        
         finalpage.style.backgroundColor = "rgb(65, 65, 65,6.5)";
         finalpage.style.backgroundImage = "url(../assets/Img/browser-nav.png)";
         finalpage.style.border = "2px solid white";
